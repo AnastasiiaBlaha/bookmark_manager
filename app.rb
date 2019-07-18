61c4c6d8f7625985f3b5702f62ce9538b3248192
AnastasiiaBlaha <anastasiia.blaha@gmail.com>
2019-07-18 15:36:05 +0100

    step 0,1,2,3

---
+++ app.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+require 'sinatra/base'
+require './lib/bookmark'
+
+# :nodoc:
+class BookmarkManager < Sinatra::Base
+  get '/' do
+    erb :index
+  end
+
+  get '/bookmarks' do
+    @bookmarks = Bookmark.all
+    erb :bookmarks
+  end
+
+  run! if app_file == BookmarkManager
+end
---
+++ spec/features/test_index_page_spec.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+feature 'View the home page' do
+  scenario 'index page' do
+    visit('/')
+    expect(page).to have_content 'Bookmarks'
+  end
+end
